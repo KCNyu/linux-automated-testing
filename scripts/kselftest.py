3355040195a0defc679971b9eaa2aafcde36ef66
KCNyu <shengyu.li.evgeny@gmail.com>
2024-04-02 17:27:49 +0300

    UPDATE: add workflow

--- scripts/kselftest.py
+++ scripts/kselftest.py
@@ -26,12 +26,22 @@ fig, axs = plt.subplots(1, 2, figsize=(14, 7))
 
 for i, (category, count) in enumerate(zip(categories, counts)):
     bar = axs[0].bar(category, count, color=colors[i], label=category)
-    axs[0].text(bar[0].get_x() + bar[0].get_width() / 2, count, f'{count}', ha='center', va='bottom', fontsize=10, fontweight='bold')
+    axs[0].text(
+        bar[0].get_x() + bar[0].get_width() / 2,
+        count,
+        f"{count}",
+        ha="center",
+        va="bottom",
+        fontsize=10,
+        fontweight="bold",
+    )
 
-axs[0].set_title("Number of Test Files", fontdict={'fontsize': 14, 'fontweight': 'bold'})
-axs[0].set_ylabel("Counts", fontdict={'fontsize': 12, 'fontweight': 'bold'})
+axs[0].set_title(
+    "Number of Test Files", fontdict={"fontsize": 14, "fontweight": "bold"}
+)
+axs[0].set_ylabel("Counts", fontdict={"fontsize": 12, "fontweight": "bold"})
 axs[0].set_xticks([])
-axs[0].set_ylabel("Counts", fontdict={'fontsize': 12, 'fontweight': 'bold'})
+axs[0].set_ylabel("Counts", fontdict={"fontsize": 12, "fontweight": "bold"})
 axs[0].tick_params(axis="x", labelsize=10)
 
 axs[0].set_xticklabels([])
@@ -41,8 +51,10 @@ axs[0].legend(loc="upper right", title="Categories")
 percentages = [count / counts[0] * 100 for count in counts[1:3]]
 percentages.append(100 - sum(percentages))
 
+
 def custom_autopct(pct):
-    return ('%1.1f%%' % pct) if pct > 0 else ''
+    return ("%1.1f%%" % pct) if pct > 0 else ""
+
 
 categories_pie = [
     "Test Files with kselftest.h",
@@ -52,10 +64,16 @@ categories_pie = [
 colors_pie = ["#344964", "#B1D9E5", "#ACB2BC"]
 
 patches, texts, autotexts = axs[1].pie(
-    percentages, labels=None, autopct=custom_autopct, startangle=140, colors=colors_pie,
-    textprops={'fontsize': 12, 'weight': 'bold'}
+    percentages,
+    labels=None,
+    autopct=custom_autopct,
+    startangle=140,
+    colors=colors_pie,
+    textprops={"fontsize": 12, "weight": "bold"},
 )
-axs[1].set_title("Percentage of Test Files", fontdict={'fontsize': 14, 'fontweight': 'bold'})
+axs[1].set_title(
+    "Percentage of Test Files", fontdict={"fontsize": 14, "fontweight": "bold"}
+)
 
 plt.tight_layout()
 plt.savefig("test_files.png")
